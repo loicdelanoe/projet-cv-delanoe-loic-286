2ec59a0e918d9fffc5a1dbeb555e699120c10301
Loïc Delanoë <loic.del4127@gmail.com>
2023-12-17 16:35:01 +0100

    :memo: Added JS burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
-console.log('test');
+const burgerMenu = {
+    mainNavElement: document.getElementById('mainNav'),
+    burgerButton: document.getElementById('burgerButton'),
+    nativeCssBurgerButton: document.querySelector('input[type="checkbox"]'),
+    navTitle: document.querySelector('.main-nav h2'),
+    init() {
+        this.generateCloseButton();
+        this.closeBurgerButton = document.getElementById('closeBurger');
+        this.nativeCssBurgerButton.classList.add('hidden');
+        this.burgerButton.classList.remove('hidden');
+        this.addEventListeners();
+    },
+    addEventListeners() {
+        this.burgerButton.addEventListener('click', () => {
+            this.mainNavElement.classList.toggle('close');
+        });
+
+        this.closeBurgerButton.addEventListener('click', () => {
+            this.mainNavElement.classList.toggle('close');
+        });
+    },
+    generateCloseButton() {
+        this.navTitle.insertAdjacentHTML('afterend', `<button id="closeBurger" class="close-burger-button">Close burger</button>`);
+    },
+};
+
+burgerMenu.init();
