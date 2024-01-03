82d82dfc52542fc3ad49d3c6d7a538999070c0ce
Loïc Delanoë <loic.del4127@gmail.com>
2024-01-03 17:07:35 +0100

    Add dropdown button and fix bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,14 @@
 const burgerMenu = {
     mainNavElement: document.getElementById('mainNav'),
     burgerButton: document.getElementById('burgerButton'),
-    nativeCssBurgerButton: document.querySelector('input[type="checkbox"]'),
+    closeBurger: document.getElementById('closeBurger'),
+    // nativeCssBurgerButton: document.querySelector('input[type="checkbox"]'),
     navTitle: document.querySelector('.main-nav h2'),
     init() {
-        this.generateCloseButton();
         this.closeBurgerButton = document.getElementById('closeBurger');
-        this.nativeCssBurgerButton.classList.add('hidden');
+        // this.nativeCssBurgerButton.classList.add('hidden');
         this.burgerButton.classList.remove('hidden');
+        this.closeBurger.classList.remove('hidden');
         this.addEventListeners();
     },
     addEventListeners() {
@@ -18,10 +19,8 @@ const burgerMenu = {
         this.closeBurgerButton.addEventListener('click', () => {
             this.mainNavElement.classList.toggle('close');
         });
-    },
-    generateCloseButton() {
-        this.navTitle.insertAdjacentHTML('afterend', `<button id="closeBurger" class="close-burger-button">Close burger</button>`);
-    },
+    }
 };
-
 burgerMenu.init();
+
+console.log(burgerMenu.burgerButton)
