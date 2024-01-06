c7028b93697015b2035e6b8f0a23195f28b93d64
Loïc Delanoë <loic.del4127@gmail.com>
2024-01-06 16:07:00 +0100

    Overhaul CSS

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,20 @@ const burgerMenu = {
     mainNavElement: document.getElementById('mainNav'),
     burgerButton: document.getElementById('burgerButton'),
     closeBurger: document.getElementById('closeBurger'),
-    // nativeCssBurgerButton: document.querySelector('input[type="checkbox"]'),
     navTitle: document.querySelector('.main-nav h2'),
     init() {
         this.closeBurgerButton = document.getElementById('closeBurger');
-        // this.nativeCssBurgerButton.classList.add('hidden');
         this.burgerButton.classList.remove('hidden');
         this.closeBurger.classList.remove('hidden');
         this.addEventListeners();
     },
     addEventListeners() {
         this.burgerButton.addEventListener('click', () => {
-            this.mainNavElement.classList.toggle('close');
+            this.mainNavElement.classList.toggle('opened');
         });
 
         this.closeBurgerButton.addEventListener('click', () => {
-            this.mainNavElement.classList.toggle('close');
+            this.mainNavElement.classList.toggle('opened');
         });
     }
 };
