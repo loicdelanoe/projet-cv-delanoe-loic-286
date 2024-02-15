bf7b07165115115451302c0722f2f5add8445329
Loïc Delanoë <95628210+loicdelanoe@users.noreply.github.com>
2024-02-15 16:15:49 +0100

    Refactor HTML and add SCSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import { burgerMenu } from './burger.js';
 import { observers } from './observers.js';
-import { dropdownButton } from './dropdown.js';
+// import { dropdownButton } from './dropdown.js';
 
 burgerMenu.init();
 observers.init();
-dropdownButton.init();
+// dropdownButton.init();
