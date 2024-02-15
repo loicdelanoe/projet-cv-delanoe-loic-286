8cd46b74a795fb2a40f520b99cac53ec67dab6b1
Loïc Delanoë <95628210+loicdelanoe@users.noreply.github.com>
2024-02-15 16:50:46 +0100

    Add SCSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-import { burgerMenu } from './burger.js';
 import { observers } from './observers.js';
-// import { dropdownButton } from './dropdown.js';
 
-burgerMenu.init();
 observers.init();
-// dropdownButton.init();
